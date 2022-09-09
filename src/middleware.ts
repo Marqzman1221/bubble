7057b2d201700bf3f4e36af6e6ac8fb89de452e5
Marqzman1221 <tgmarquise@gmail.com>
2022-09-08 23:45:24 -0400

    Added auth providers and auth middleware

---
+++ src/middleware.ts
@@ -0,0 +1,11 @@
+import withAuth from 'next-auth/middleware'
+
+// export { default } from 'next-auth/middleware'
+
+export default withAuth(function middleware(req) {
+  console.log(req.nextauth.token)
+})
+
+export const config = {
+  matcher: ['/', '/logout', '/account-verification', '/create-account'],
+}
